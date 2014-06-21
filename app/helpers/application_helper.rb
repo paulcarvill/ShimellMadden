047c849129bb9fd1452d67b7d89da7e4fc835e30
Paul Carvill <paul@paulcarvill.com>
2014-06-21 17:20:54 +0100

    project list pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,28 @@ module ApplicationHelper
   def action?(*action)
     action.include?(params[:action])
   end
-  
+
+  def nav()
+  	if controller?("projects") && action?("index")
+  		"Projects"
+  	elsif controller?("projects") && action?("show")
+  		"Project"
+  	elsif controller?("items") && action?("index")
+  		"Items"
+  	elsif controller?("items") && action?("show")
+  		"Item"
+  	elsif controller?("collections") && action?("index")
+  		"Collections"
+  	elsif controller?("collections") && action?("show")
+  		"Collection"
+  	elsif controller?("items") && action?("archive", "archiveshow")
+  		"Archive"
+  	elsif controller?("pages") && action?("about", "show")
+  		"Information"
+  	elsif controller?("pages") && action?("shop")
+  		"Shop"
+  	else
+  		"Menu"
+  	end
+  end
 end
