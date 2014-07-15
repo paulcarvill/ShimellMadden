0905c417c8ea2c3996b1c7e3949ba6f59783dc6b
Paul Carvill <paul@paulcarvill.com>
2014-07-15 23:03:40 +0100

    made news items multi-image. includes creating carousels on infinite scroll load.

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,7 +101,10 @@ Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ex
 	{
 		headline: "News headline twelve",
 		body: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p><p>
-Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
+		blogImage1: File.open(Rails.root.join("db/seeds/news","news3.jpg")),
+		blogImage2: File.open(Rails.root.join("db/seeds/news","news2.jpg")),
+		blogImage3: File.open(Rails.root.join("db/seeds/news","news1.jpg"))
 	},
 	{
 		headline: "News headline thirteen",
